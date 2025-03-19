bde014924501364222ee74b8d9162e9bcc284641
Shagun Sharma <shagun.sharma@kellogg.northwestern.edu>
2025-03-19 09:14:25 +0000

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :current_user
+  helper_method :current_user  
 
   def current_user
-    puts "------------------ code before every request ------------------"
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def require_login
+    unless current_user  
+      flash[:notice] = "You must be logged in to access this page!"
+      redirect_to "/login"
+    end
   end
 end
